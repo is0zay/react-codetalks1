73468cc8ed5e62157d1033a2bf3669ead2980c48
Isaiah <isozay0@gmail.com>
2023-07-03 14:05:03 -0400

    up

--- q6.js
+++ q6.js
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router';
 
+
+// Create an HTTP server and set up a request listener
 http
 	.createServer((req,res) => {
 		// This context object contains results of the render
 		const context = {};
 
+		//Render the component to html
 		const html = ReactDOMServer.renderToString(
 			<StaticRouter location={req.url} context={context}>
 				<App />
@@ -20,9 +23,12 @@ http
 				Location: context.url
 			});
 			res.end();
+		//otherwise render the html
 		} else {
 			res.write(html);
 			res.end();
 		}
 	})
 	.listen(3000);
+
+	// Redirect  allows you to specify a target URL to which the user should be redirected when the <Redirect> component is rendered.
